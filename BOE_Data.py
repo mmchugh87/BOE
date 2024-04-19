7e5219c406c03a303dab2365de6257ba3594a6f6
harry_hindsight <66922291+mmchugh87@users.noreply.github.com>
2024-04-19 21:01:01 +0100

    Add files via upload

--- BOE_Data.py
+++ BOE_Data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[89]:
+# In[99]:
 
 
 # -------------------------------------------------------------------------------
@@ -14,86 +14,19 @@ from scipy.stats import zscore
 import pickle
 
 # -------------------------------------------------------------------------------
-# Load and combine each tab from the source xlsx file
+# Import additional functions from our own BOE_Utilities module
 # -------------------------------------------------------------------------------
 
-def create_combined_dataframe(file_name):
-    try:
-        # Obtain list of sheet names ("tabs") from the source Excel file.
-        print("Sheet names in the Excel file:")
-        excel_file = pd.ExcelFile(file_name)
-        sheet_names = excel_file.sheet_names
-        print(sheet_names)
-        
-        dfs = []  # List to store dataframes from each sheet
-        
-        # Iterate through each sheet in the Excel file
-        for sheet_name in sheet_names:
-            print(f"Processing sheet: {sheet_name}")
-            # Read the sheet into a pandas dataframe
-            # header = 4, based on visual inspection of the excel file
-            df = pd.read_excel(file_name, sheet_name=sheet_name, header=4)
-            
-            # Slice off a single, superfluous row from the resulting dataframe (based on visual inspection of file)
-            df = df[1:]
-            
-            # The "time period" column shall be the index for each dataframe.
-            df.set_index('Time period and dataset code row', inplace=True)
-            clean_index_name = "TimePeriod"
-            df.index.name = clean_index_name
-            
-            # Rename columns with suffix indicating the source sheet
-            df.columns = [f'{col} (Sheet_{sheet_name})' for col in df.columns]
-        
-            # add the newly loaded dataframe to the list of dataframes ("dfs")
-            dfs.append(df)
-            print("Dataframe loaded successfully.")
-                
-        # Concatenate all dataframes into a single dataframe
-        dfs_combined = pd.concat(dfs, axis=1, join='outer')
-        print(f"{len(dfs)} Dataframes combined successfully.")
-        return dfs_combined
-        
-    except Exception as e:
-        print(f"Error loading Excel data: {str(e)}")
-        return None
-    
-# Run the function: create_combined_dataframe
+import BOE_Utilities
+
+# -------------------------------------------------------------------------------
+# Use BOE_Utilities to create a fully combined and transformed dataframe
+# -------------------------------------------------------------------------------
+
+# Run a chain of BOE_Utilities functions together on the source file
 file_name = 'Dashboard dataset.xlsx'
-df_GDP = create_combined_dataframe(file_name)
 
-# -------------------------------------------------------------------------------
-# Tidy the resulting combined dataframe
-# -------------------------------------------------------------------------------
-
-def tidy_the_dataframe(df):
-
-    try:
-        # Convert the format of the index to be easily recognized by Python as "datetime" format
-        df.index = df.index.str.replace(' ', '-')  # Replace spaces with dashes
-        df.index = pd.to_datetime(df.index)        # Convert index to datetime format
-
-        print("Index converted to datetime format successfully.")
-    except Exception as e:
-        print(f"Error occurred while converting index to datetime format: {e}")
-
-    try:
-        # Set datatype for all columns to floating
-        df = df.apply(pd.to_numeric, errors='coerce').astype(float)
-
-        print("Columns converted to floating point numbers successfully.")
-    except Exception as e:
-        print(f"Error occurred while converting columns to floating point numbers: {e}")
-
-    return df
-
-# Run the function: tidy_the_dataframe
-df_GDP = tidy_the_dataframe(df_GDP)
-
-# -------------------------------------------------------------------------------
-# Through manual inspection, propose alternative column names for clarity / brevity
-# -------------------------------------------------------------------------------
-
+# Based on a manual review of the source files, define alternative column names for brevity / clarity
 column_mapping = {'Households: Final consumption expenditure (Sheet_GDP)': 'GDP_Component_Household_Spend', 
                   'General Government: Final consumption expenditure (Sheet_GDP)': 'GDP_Component_Gov_Spend',
                   'Gross fixed capital formation: Gross capital formation (Sheet_GDP)': 'GDP_Component_GFCF',
@@ -108,54 +41,33 @@ column_mapping = {'Households: Final consumption expenditure (Sheet_GDP)': 'GDP_
                   'Services: UK Domestic (Sheet_Consumption)': 'Household_Component_Services',
                   'Other (Sheet_Consumption)': 'Household_Component_Other'}
 
-def rename_columns(df, column_mapping):
-
-    try:
-        # Apply column renaming using the provided mapping
-        df = df.rename(columns=column_mapping)
-        print("Columns renamed successfully.")
-        return df
+# Define and then execute a chain of functions from our BOE_Utilities module.
+# This chain of functions receives the xlsx file and the column_mapping, and then combines and transforms the xlsx data...
+#...into a dataframe that will subsequently be used to generate other useful dataframes and plots.
+def create_df_gdp(file_name, column_mapping):
+    df = create_combined_dataframe(file_name)
+    df = tidy_the_dataframe(df)
+    df = rename_columns(df, column_mapping)
+    return df
     
-    except Exception as e:
-        print("An error occurred while renaming columns:", e)
-        return None
-
-# Run the function: rename_columns
-df_GDP = rename_columns(df=df_GDP, column_mapping=column_mapping)
+# Run the chain of functions and assign the resulting dataframe
+df_GDP = create_df_gdp(file_name=file_name, column_mapping=column_mapping)
 
 # -------------------------------------------------------------------------------
-# Manually identify meaningful groupings of columns
+# Manually identify meaningful groupings of columns (used in subsequent plots)
 # -------------------------------------------------------------------------------
 
-# Define a list of the columns that represent top level *components* of GDP.
+# Define a list of the columns that are the top-level *components* of GDP.
 GDP_Components = list(df_GDP.columns)[:6]
+# This list will be used later, when creating plots
 
-# Define a list of the columns that represent *components* of Household Spending.
+# Define a list of the columns that are the top-level *components* of Household Spend.
 Household_Components = list(df_GDP.columns)[-5:]
+# This list will be used later, when creating plots
 
 # -------------------------------------------------------------------------------
-# Create and tweak duplicate dataframes, to feed various data visualisations later
+# Use BOE_Utilities to create duplicate dataframes containing % change values (used in subsequent plots)
 # -------------------------------------------------------------------------------
-
-def create_percentage_change_df(df, shift_value):
-    try:
-        # Copy the input DataFrame
-        df_copy = df.copy()
-        
-        # Calculate percentage changes versus the specified shift value
-        df_copy = ((df_copy / df_copy.shift(shift_value)) - 1) * 100
-        
-        # Calculate z-score for the 'GDP_Total_MarketPrices' column
-        df_copy['Zscore'] = zscore(df_copy['GDP_Total_MarketPrices'], nan_policy='omit')
-        
-        # Clip z-score values
-        df_copy['Zscore'] = np.clip(df_copy['Zscore'], -4, 4)
-        
-        # Return the resulting DataFrame
-        return df_copy
-    except Exception as e:
-        print(f"An error occurred whilst creating percentage change df: {e}")
-        return None
 
 # Run function to create duplicate dataframe containing percentage changes versus preceding quarter.
 df_GDP_QvPriorQ = create_percentage_change_df(df_GDP, 1)
@@ -164,15 +76,16 @@ df_GDP_QvPriorQ = create_percentage_change_df(df_GDP, 1)
 df_GDP_QvPriorY = create_percentage_change_df(df_GDP, 3)
 
 # -------------------------------------------------------------------------------
+# Create a duplicate df containing absolute values of GDP components (used in subsequent plots)
+# -------------------------------------------------------------------------------
 
-# Create duplicate df that takes absolute values of each top-level GDP component, for usage in subsequent visualisation.
 df_GDPComponents_Abs = abs(df_GDP[GDP_Components])
 row_sums = df_GDPComponents_Abs.sum(axis=1)
 df_GDPComponents_Abs = df_GDPComponents_Abs.div(row_sums, axis=0) * 100
 # Note: the resulting dataframe calculates what proportion of the total absolute GDP values each component contributes.
 
 # -------------------------------------------------------------------------------
-# Create and tweak a duplicate dataframe to feed a "treemap" later. A bit tricky.
+# Create and tweak a duplicate dataframe to feed a "treemap" (used in subsequent plots)
 # -------------------------------------------------------------------------------
 
 # Select the most recent row
@@ -222,7 +135,7 @@ df_treemap['Parent_Component'] = df_treemap['Parent_Component'].replace(values_t
 df_treemap['Component'] = df_treemap['Component'].str.replace('_', '<br>')
 
 # -------------------------------------------------------------------------------
-# Manually define the bundle of dataframes and lists to "pickle", to feed the dashboard.
+# Bundle the dataframes and lists created above, and "pickle" them. This bundle will feed the dashboard.
 # -------------------------------------------------------------------------------
 
 data_bundle = {
---
+++ BOE_Utilities.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# -------------------------------------------------------------------------------
+# Define function that loads and combines tabs from the source xlsx file
+# -------------------------------------------------------------------------------
+
+def create_combined_dataframe(file_name):
+    try:
+        # Obtain list of sheet names ("tabs") from the source Excel file.
+        print("Sheet names in the Excel file:")
+        excel_file = pd.ExcelFile(file_name)
+        sheet_names = excel_file.sheet_names
+        print(sheet_names)
+        
+        dfs = []  # List to store dataframes from each sheet
+        
+        # Iterate through each sheet in the Excel file
+        for sheet_name in sheet_names:
+            print(f"Processing sheet: {sheet_name}")
+            # Read the sheet into a pandas dataframe
+            # header = 4 (based on  manual inspection of file)
+            df = pd.read_excel(file_name, sheet_name=sheet_name, header=4)
+            
+            # Slice off a single, superfluous row from the resulting dataframe (based on manual inspection of file)
+            df = df[1:]
+            
+            # The "time period" column shall be the index for each dataframe (based on  manual inspection of file)
+            df.set_index('Time period and dataset code row', inplace=True)
+            clean_index_name = "TimePeriod"
+            df.index.name = clean_index_name
+            
+            # Rename columns with suffix indicating the source sheet
+            df.columns = [f'{col} (Sheet_{sheet_name})' for col in df.columns]
+        
+            # add the newly loaded dataframe to the list of dataframes ("dfs")
+            dfs.append(df)
+            print("Dataframe loaded successfully.")
+                
+        # Concatenate all dataframes into a single dataframe
+        dfs_combined = pd.concat(dfs, axis=1, join='outer')
+        print(f"{len(dfs)} Dataframes combined successfully.")
+        return dfs_combined
+        
+    except Exception as e:
+        print(f"Error loading Excel data: {str(e)}")
+        return None
+
+# -------------------------------------------------------------------------------
+# Define function that tidies up the combined dataframe
+# -------------------------------------------------------------------------------
+
+def tidy_the_dataframe(df):
+
+    try:
+        # Convert the format of the index to be easily recognized by Python as "datetime" format
+        df.index = df.index.str.replace(' ', '-')  # Replace spaces with dashes
+        df.index = pd.to_datetime(df.index)        # Convert index to datetime format
+
+        print("Index converted to datetime format successfully.")
+    except Exception as e:
+        print(f"Error occurred while converting index to datetime format: {e}")
+
+    try:
+        # Set datatype for all columns to floating
+        df = df.apply(pd.to_numeric, errors='coerce').astype(float)
+
+        print("Columns converted to floating point numbers successfully.")
+    except Exception as e:
+        print(f"Error occurred while converting columns to floating point numbers: {e}")
+
+    return df
+
+# -------------------------------------------------------------------------------
+# Define function that amends the column names of the combined dataframe
+# -------------------------------------------------------------------------------
+
+def rename_columns(df, column_mapping):
+
+    try:
+        # Apply column renaming using the provided mapping
+        df = df.rename(columns=column_mapping)
+        print("Columns renamed successfully.")
+        return df
+    
+    except Exception as e:
+        print("An error occurred while renaming columns:", e)
+        return None
+    
+# -------------------------------------------------------------------------------
+# Define function that creates duplicate dataframes that show values as % change
+# -------------------------------------------------------------------------------
+
+def create_percentage_change_df(df, shift_value):
+    try:
+        # Copy the input DataFrame
+        df_copy = df.copy()
+        
+        # Calculate percentage changes versus the specified shift value
+        df_copy = ((df_copy / df_copy.shift(shift_value)) - 1) * 100
+        
+        # Calculate z-score for the 'GDP_Total_MarketPrices' column
+        df_copy['Zscore'] = zscore(df_copy['GDP_Total_MarketPrices'], nan_policy='omit')
+        
+        # Clip z-score values
+        df_copy['Zscore'] = np.clip(df_copy['Zscore'], -4, 4)
+        
+        # Return the resulting DataFrame
+        return df_copy
+    except Exception as e:
+        print(f"An error occurred whilst creating percentage change df: {e}")
+        return None
+
